51b0d9d07c2fc9fc179a82d7d60768e466815830
DoTuanNghia <dtn752k4@gmail.com>
2025-10-02 15:40:23 +0700

    Initial commit

---
+++ src/main/java/vn/hoidanit/todo/model/Todo.java
@@ -0,0 +1,39 @@
+package vn.hoidanit.todo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String username;
+
+    boolean isCompleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
