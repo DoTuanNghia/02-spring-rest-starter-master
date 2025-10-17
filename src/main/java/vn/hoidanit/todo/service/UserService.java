0a5b341674f20876b4908aa12868d265b3a43b68
DoTuanNghia <dtn752k4@gmail.com>
2025-10-17 23:14:18 +0700

    #72 Format response

---
+++ src/main/java/vn/hoidanit/todo/service/UserService.java
@@ -0,0 +1,18 @@
+package vn.hoidanit.todo.service;
+
+import vn.hoidanit.todo.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User createUser(User user);
+
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    User updateUser(Long id, User updatedUser);
+
+    void deleteUser(Long id);
+}
